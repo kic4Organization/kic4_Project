e2f76a5b7636c38f3ee047631596c51148bc9f3c
RedNine97 <ghdrn221@gmail.com>
2023-02-21 19:49:19 +0900

    commit

--- KIC4_Project/src/main/java/action/U_LoginPageProcAction.java
+++ KIC4_Project/src/main/java/action/U_LoginPageProcAction.java
@@ -1,25 +1,27 @@
 package action;
 
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import member.MemberDBMgr;
 
 public class U_LoginPageProcAction implements CommandAction {
 
-	//@Override
+	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		//LoginPage.shop에 의한 LoginPage.jsp에서 값이 넘어옴.
-		String memid=request.getParameter("memberid");
-		String pwd=request.getParameter("memberpw");
-		
-		System.out.println("memid=>"+memid);
-		System.out.println("pwd=>"+pwd);//경로확인
-		MemberDBMgr memMgr = new MemberDBMgr();
-		boolean loginCheck = memMgr.loginCheck(memid,pwd);
-		
-		request.setAttribute("loginCheck", new Boolean(loginCheck)); 
-		request.setAttribute("memid", new String(memid));
-		
-		return "/U_LoginPageProc.jsp";
+				String memid=request.getParameter("memberid");
+				String pwd=request.getParameter("memberpw");
+				
+				System.out.println("memid=>"+memid);
+				System.out.println("pwd=>"+pwd);//경로확인
+				MemberDBMgr memMgr = new MemberDBMgr();
+				boolean loginCheck = memMgr.loginCheck(memid,pwd);
+				
+				request.setAttribute("loginCheck", new Boolean(loginCheck)); 
+				request.setAttribute("memid", new String(memid));
+				
+				return "/U_LoginPageProc.jsp";
 	}
 
 }

11bacff181f489ef73ce6656bfa7540c91b990e5
RedNine97 <ghdrn221@gmail.com>
2023-02-21 20:31:33 +0900

    ?

--- KIC4_Project/src/main/webapp/script.js
+++ KIC4_Project/src/main/webapp/script.js
@@ -1,84 +1,76 @@
 function loginCheck(){
-	if(document.login.memid.value==""){
+	if(document.login.mem_id.value==""){
 		alert("아이디를를 입력해 주세요.");
 		document.login.mem_id.focus();
 		return;
 	}
-	if(document.login.pwd.value==""){
+	if(document.login.mem_passwd.value==""){
 		alert("비밀번호를 입력해 주세요.");
-		document.login.pwd.focus();
+		document.login.mem_passwd.focus();
 		return;
 	}
 	document.login.submit();
 }
-
+//회원가입
 function memberReg(){
-	document.location="agreement.shop";
+	//document.location="Register.jsp";
+	//document.location="agreement.jsp";
+	document.location="U_agreement.shop";
 }
 
+//회원가입을 할때 각 항목을 제대로 입력->체크
 function inputCheck(){
-	if(document.regForm.memid.value==""){
+	if(document.regForm.mem_id.value==""){
 		alert("아이디를 입력해 주세요.");
-		document.regForm.memid.focus();
+		document.regForm.mem_id.focus();
 		return;
 	}
-	if(document.regForm.pwd.value==""){
+	if(document.regForm.mem_passwd.value==""){
 		alert("비밀번호를 입력해 주세요.");
-		document.regForm.pwd.focus();
+		document.regForm.mem_passwd.focus();
 		return;
 	}
-	if(document.regForm.repwd.value==""){
+	if(document.regForm.mem_repasswd.value==""){
 		alert("비밀번호를 확인해 주세요");
-		document.regForm.repwd.focus();
+		document.regForm.mem_repasswd.focus();
 		return;
 	}
-	if(document.regForm.memname.value==""){
+	if(document.regForm.mem_name.value==""){
 		alert("이름을 입력해 주세요.");
-		document.regForm.memname.focus();
+		document.regForm.mem_name.focus();
 		return;
 	}
-	//주민번호 체크 구문은 삭제함
-	if(document.regForm.email.value==""){
+	if(document.regForm.mem_email.value==""){
 		alert("이메일을 입력해 주세요.");
-		document.regForm.email.focus();
+		document.regForm.mem_email.focus();
 		return;
 	}
-	if(document.regForm.mphone.value==""){
+	if(document.regForm.mem_phone.value==""){
 		alert("연락처를 입력해 주세요.");
-		document.regForm.mphone.focus();
+		document.regForm.mem_phone.focus();
 		return;
 	}
 	
-	if(document.regForm.pwd.value != document.regForm.repwd.value){
+	if(document.regForm.mem_passwd.value != document.regForm.mem_repasswd.value){
 		alert("비밀번호가 일치하지 않습니다.");
-		document.regForm.repwd.focus();
+		document.regForm.mem_repasswd.focus();
 		return;
 	}
 	document.regForm.submit();
 }
-
-//중복ID체크 해주는 자바스크립트함수 선언
-
-
-/*
+//Register.jsp->script.js(idCheck->IdCheck.jsp?
 function idCheck(id){
-	if(id==""){
-		alert("아이디를 먼저 입력하세요!")
-	//document.폼객체명.입력양식 객체명. 함수명(~)
-		document.regForm.mem_id.focus();
-	}else{//입력했다면, jsp파일에게 매개변수를 전달
-		url="IdCheck.jsp?mem_id="+id;
-		//request.getParameter("mem_id")
-		//1.불러올 문서명 2. 창의 제목 3. 창의 옵션(위치, 크기 지정)
-   		window.open(url,"post","left=450,top=150,width=300,height=150");
+    if(id==""){
+          alert("아이디를 입력해 주세요!");
+		  document.regForm.mem_id.focus();
+	}else{
+         url="IdCheck.jsp?mem_id="+id;
+window.open(url,"post","width=300,height=150");
 	}
 }
-*/
-//우편번호를 검색해주는 함수선언
+
+//우편번호창을 열 수 있도록 함수호출
 function zipCheck(){
-    //check=y->검색 전의 창의 모습을 구분하는 인사(매개변수로 지정해서 전달)하기 위해서
-    //menubar(메뉴바), status(상태바), scrollbars(스크롤기능), toolbar=yes|no
-     url="ZipCheck.jsp?check=y";
-     window.open(url,"post","left=400,top=220,width=500,height=300,"+
-     								"menubar=no,status=yes,toolbar=no,scrollbars=yes")
+ url="U_ZipCheck.jsp?check=y";
+ window.open(url,"post","toolbar=no,width=500,height=300,directories=no,status=yes,scrollbars=yes,menubar=no");
 }
